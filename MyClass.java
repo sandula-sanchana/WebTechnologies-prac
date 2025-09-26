157d91d6df552559a286195c5cf723003455518c
sandula-sanchana <sandulasanchana@gmail.com>
2025-09-26 14:05:02 +0530

    let,const

---
+++ MyClass.java
@@ -0,0 +1,13 @@
+public class MyClass {
+    {
+        System.out.println("heyy");
+    }
+
+    MyClass(){
+        System.out.println("byee");
+    }
+
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+    }
+}
